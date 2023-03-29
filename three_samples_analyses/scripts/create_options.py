275f0cb509a3616509ef9f0311963cbfa372b275
Andrea Cossa <IEO5505@Cossas-MacBook-Pro.local>
2023-03-29 18:30:09 +0200

    change options and config

--- three_samples_analyses/scripts/create_options.py
+++ three_samples_analyses/scripts/create_options.py
@@ -4,12 +4,12 @@ import pandas as pd
 from itertools import product
 
 # Lists
-samples = ['MDA']# , 'AML']#, 'PDX']
-input_mode = ['less_stringent']#, 'more_stringent']
-filtering = ['miller2022']#, 'MQuad', 'velten2021', 'CV', 'pegasus', 'seurat', 'density']
-dimred = ['no_dimred']#, 'more_stringent']
-models = [ 'logit'] #, 'kNN', 'SVM, 'xgboost']
-min_cell_number = [0]#, 10, 50]
+samples = ['MDA', 'AML']#, 'PDX']
+input_mode = ['more_stringent']
+filtering = ['miller2022', 'MQuad', 'ludwig2019', 'pegasus']
+dimred = ['no_dimred', 'PCA', 'UMAP', 'diffmap'] 
+models = [ 'logit', 'kNN', 'xgboost']
+min_cell_number = [10]
 
 # Product, and write
 jobs = list(product(samples, input_mode, filtering, dimred, models, min_cell_number)) 
